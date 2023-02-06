6317e3cd06e9d224b9cec2a078d6df2aecdcd219
Nicolas Lau <nicolas.lau@zalaris.com>
2023-02-06 16:09:37 +0000

    clean up

--- srv/provisioning.js
+++ srv/provisioning.js
@@ -1,18 +1,17 @@
-const cds = require('@sap/cds');
-const xsenv = require('@sap/xsenv');
-xsenv.loadEnv();
+const xsenv = require('@sap/xsenv')
+xsenv.loadEnv()
 
 module.exports = (service) => {
 
     service.on('dependencies', async (req, next) => {
-        let dependencies = await next();
+        let dependencies = await next()
         const services = xsenv.getServices({
             registry: { tag: 'SaaS' },
             destination: { tag: 'destination' }
-        });
-        dependencies.push({ xsappname: services.destination.xsappname });
-        console.log(">>> SaaS Dependencies:", JSON.stringify(dependencies));
-        return dependencies;
-    });
+        })
+        dependencies.push({ xsappname: services.destination.xsappname }) //adds the subscriber destination as dependency
+        console.log(">>> SaaS Dependencies:", JSON.stringify(dependencies))
+        return dependencies
+    })
 
 }
--- srv/server.js
+++ srv/server.js
@@ -1,27 +1,15 @@
 const cds = require("@sap/cds")
-const xsenv = require("@sap/xsenv")
-
-if (process.env.NODE_ENV) {
-    xsenv.loadEnv()
-    const services = xsenv.getServices({
-        dest: { tag: "destination" }
-    })
-    console.log('>>> Service dest name: ' + services.dest.xsappname)
-    cds.env.mtx.dependencies = [services.dest.xsappname]
-}
-
 
 cds.on('served', async () => {
     const { 'cds.xt.SaasProvisioningService': provisioning } = cds.services
     // Add provisioning logic only if multitenancy is there..
-    if(provisioning){
-        let tenantProvisioning = require('./provisioning');
-        provisioning.prepend(tenantProvisioning);
+    if (provisioning) {
+        let tenantProvisioning = require('./provisioning')
+        provisioning.prepend(tenantProvisioning)
+    } else {
+        console.log(">>> There is no service, therefore does not serve multitenancy!")
     }
-    else{
-        console.log("There is no service, therefore does not serve multitenancy!");
-    }
-  })
+})
 
 module.exports = cds.server
 
